2c553e5d260db60af6fea36f3747ff0a245dccff
Shariff Inayat <shariff.inayat@gmail.com>
2014-12-16 16:30:14 +0000

    Indent with 4 spaces everywhere

--- testapps/views.py
+++ testapps/views.py
@@ -2,16 +2,16 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 
 def index(request):
-  return HttpResponse("Testapp index listing.")
+    return HttpResponse("Testapp index listing.")
 
 def show(request, testapp_id):
-  return HttpResponse("Showing testapp %s" % testapp_id)
+    return HttpResponse("Showing testapp %s" % testapp_id)
 
 def slashed(request, testapp_id):
-  return HttpResponse("Showing testapp %s at url that should be / terminated." % testapp_id)
+    return HttpResponse("Showing testapp %s at url that should be / terminated." % testapp_id)
 
 def plused(request):
-  return HttpResponse("Showing URLs with plus signs are not re-encoded")
+    return HttpResponse("Showing URLs with plus signs are not re-encoded")
 
 def quoted(request):
-  return HttpResponse("Showing URLs with url-encoding are not re-encoded")
+    return HttpResponse("Showing URLs with url-encoding are not re-encoded")
--- unslashed/test.py
+++ unslashed/test.py
@@ -6,59 +6,59 @@ from django.middleware.common import CommonMiddleware
 from unslashed.middleware import RemoveSlashMiddleware
 
 class RemoveSlashMiddlewareTest(TestCase):
-  def setUp(self):
-    self.client = Client()
+    def setUp(self):
+        self.client = Client()
 
-  def test_permanent_redirect_to_non_slashed(self):
-    response = self.client.get('/testapps/', follow=False)
-    self.assertIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertFalse(response['Location'].endswith('/'))
+    def test_permanent_redirect_to_non_slashed(self):
+        response = self.client.get('/testapps/', follow=False)
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertFalse(response['Location'].endswith('/'))
 
-    response = self.client.get('/testapps/1/', follow=False)
-    self.assertIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertFalse(response['Location'].endswith('/'))
+        response = self.client.get('/testapps/1/', follow=False)
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertFalse(response['Location'].endswith('/'))
 
-  def test_permanent_redirect_to_unslashed_when_url_has_plus_signs(self):
-    response = self.client.get('/testapps/url+with+plus/', follow=False)
-    self.assertIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertFalse(response['Location'].endswith('/'))
-    self.assertRegexpMatches(response['Location'], r'/url\+with\+plus$')
+    def test_permanent_redirect_to_unslashed_when_url_has_plus_signs(self):
+        response = self.client.get('/testapps/url+with+plus/', follow=False)
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertFalse(response['Location'].endswith('/'))
+        self.assertRegexpMatches(response['Location'], r'/url\+with\+plus$')
 
-    response = self.client.get('/testapps/url+with+plus/?param1=1&param2=%2Bfoo', follow=False)
-    self.assertIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertRegexpMatches(response['Location'], r'/url\+with\+plus\?param1=1&param2=%2Bfoo$')
+        response = self.client.get('/testapps/url+with+plus/?param1=1&param2=%2Bfoo', follow=False)
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertRegexpMatches(response['Location'], r'/url\+with\+plus\?param1=1&param2=%2Bfoo$')
 
-  def test_permanent_redirect_to_unslashed_when_url_has_urlencoded_chars(self):
-    # urlquote is used because django.test.Client decodes the URL
-    # which goes against the test
-    slashed_url = urlquote('/testapps/quoted/foo%2Bbar%23baz%20/')
-    response = self.client.get(slashed_url, follow=False)
-    self.assertIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertFalse(response['Location'].endswith('/'))
-    self.assertRegexpMatches(response['Location'], r'/quoted/foo%2Bbar%23baz%20$')
+    def test_permanent_redirect_to_unslashed_when_url_has_urlencoded_chars(self):
+        # urlquote is used because django.test.Client decodes the URL
+        # which goes against the test
+        slashed_url = urlquote('/testapps/quoted/foo%2Bbar%23baz%20/')
+        response = self.client.get(slashed_url, follow=False)
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertFalse(response['Location'].endswith('/'))
+        self.assertRegexpMatches(response['Location'], r'/quoted/foo%2Bbar%23baz%20$')
 
-    response = self.client.get(slashed_url + '?param1=1&param2=%2Btest', follow=False)
-    self.assertIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertRegexpMatches(response['Location'], r'/quoted/foo%2Bbar%23baz%20\?param1=1&param2=%2Btest$')
+        response = self.client.get(slashed_url + '?param1=1&param2=%2Btest', follow=False)
+        self.assertIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertRegexpMatches(response['Location'], r'/quoted/foo%2Bbar%23baz%20\?param1=1&param2=%2Btest$')
 
-  def test_no_redirect_when_slash_url_is_valid(self):
-    response = self.client.get('/testapps/1/urlendsinslash/', follow=False)
-    self.assertIsInstance(response, HttpResponse)
-    self.assertEqual(response.status_code, 200)
+    def test_no_redirect_when_slash_url_is_valid(self):
+        response = self.client.get('/testapps/1/urlendsinslash/', follow=False)
+        self.assertIsInstance(response, HttpResponse)
+        self.assertEqual(response.status_code, 200)
 
-  def test_no_redirect_when_slashed_and_unslashed_invalid(self):
-    response = self.client.get('/testapps/invalid/', follow=False)
-    self.assertNotIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertEqual(response.status_code, 404)
+    def test_no_redirect_when_slashed_and_unslashed_invalid(self):
+        response = self.client.get('/testapps/invalid/', follow=False)
+        self.assertNotIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertEqual(response.status_code, 404)
 
-    response = self.client.get('/testapps/invalid', follow=False)
-    self.assertNotIsInstance(response, HttpResponsePermanentRedirect)
-    self.assertEqual(response.status_code, 404)
+        response = self.client.get('/testapps/invalid', follow=False)
+        self.assertNotIsInstance(response, HttpResponsePermanentRedirect)
+        self.assertEqual(response.status_code, 404)
 
-  def test_warns_about_redirect_and_post(self):
-    with self.settings(DEBUG=True):
-      with self.assertRaises(RuntimeError):
-        self.client.post('/testapps/', follow=False)
+    def test_warns_about_redirect_and_post(self):
+        with self.settings(DEBUG=True):
+            with self.assertRaises(RuntimeError):
+                self.client.post('/testapps/', follow=False)
 
-  def tearDown(self):
-    del self.client
+    def tearDown(self):
+        del self.client
