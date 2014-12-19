dfe1eb0d93b65fe0079ef086b86a54fa29b89504
Dalton Hubble <dghubble@gmail.com>
2014-12-18 20:27:07 -0800

    Update setup.py to match recent recommendations
    * Require Django 1.5 or higher

--- setup.py
+++ setup.py
@@ -1,34 +1,36 @@
 import os
+import codecs
+from setuptools import setup
 from unslashed import __version__
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+here = os.path.abspath(os.path.dirname(__file__))
 
-
-def read(filename):
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+# Get the long description from the relevant file
+with codecs.open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    # Required information
-    name = 'django-unslashed',
-    version = __version__,
-    author = 'Dalton Hubble',
-    author_email = 'dghubble@gmail.com',
+    name='django-unslashed',
+
+    # Versions should comply with PEP440. For single-sourced versioning, see
+    # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
+    version=__version__,
+
+    description='Django Middleware that can automatically remove trailing URL slashes and 301 redirect to the non-slash-terminated URL.',
+    long_description=long_description,
+
+    # The project URL
     url = 'https://github.com/dghubble/django-unslashed',
 
-    packages = ['unslashed'],
+    # Author details
+    author='Dalton Hubble',
+    author_email='dghubble@gmail.com',
 
-    # Package dependencies
-    install_requires = ['Django>=1.0'],
+    # Choose your license
+    license='MIT License',
 
-    # Metadata
-    license = 'MIT License',
-    keywords = 'django slash remove trailing unslash remove_slash path',
-    description = 'Django Middleware that can automatically remove trailing URL slashes and 301 redirect to the non-slash-terminated URL.',
-    long_description = read('README.md'),
-    classifiers = [
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -39,4 +41,23 @@ setup(
         'Framework :: Django',
         'Environment :: Web Environment',
         'Topic :: Internet :: WWW/HTTP'],
+
+    # What does your project relate to?
+    keywords='django slash remove trailing unslash remove_slash path',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['unslashed'],
+
+    # Run-time package dependencies. These will be installed by pip when your
+    # project is installed.
+    install_requires=['Django>=1.5'],
+
+    # MANIFEST.in included entries should be included as package data and
+    # installed into site-packages 
+    include_package_data=True,
+
+    # Default to False unless you specifically intend the package to be
+    # installed as an .egg
+    zip_safe=False,
 )
